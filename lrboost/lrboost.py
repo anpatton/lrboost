7e7dccfaa80011540b7b8f2f55630d60b1c12504
Nathan Walker <nathanedwardwalker@gmail.com>
2022-01-06 00:04:10 -0500

    formatting
    I use black (the best formatter)

--- lrboost/lrboost.py
+++ lrboost/lrboost.py
@@ -4,8 +4,11 @@ import numpy as np
 from sklearn.utils.validation import check_is_fitted
 from typing import Dict
 
-class LRBoostRegressor():
-    def __init__(self, linear_model=RidgeCV(), non_linear_model=HistGradientBoostingRegressor()):
+
+class LRBoostRegressor:
+    def __init__(
+        self, linear_model=RidgeCV(), non_linear_model=HistGradientBoostingRegressor()
+    ):
         self.linear_model = linear_model
         self.non_linear_model = non_linear_model
 
@@ -13,7 +16,7 @@ class LRBoostRegressor():
         """Internal sklearn helper that indicates the object has been fitted
 
         Returns:
-            bool: True 
+            bool: True
         """
         return True
 
@@ -32,7 +35,7 @@ class LRBoostRegressor():
         linear_prediction = self.linear_model.predict(X)
         linear_residual = np.subtract(linear_prediction, y)
         self.non_linear_model.fit(X, y=linear_residual, sample_weight=sample_weight)
-        
+
         return self
 
     def predict(self, X) -> np.array:
